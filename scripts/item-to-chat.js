e3d424bb2bf433ec20db4a9503dda026118c6ff9
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2022-07-28 14:16:50 -0400

    Make it so there is no token necessary

--- scripts/item-to-chat.js
+++ scripts/item-to-chat.js
@@ -7,12 +7,12 @@ Hooks.on('getItemSheetPF2eHeaderButtons', (sheet, buttons) => {
             if (sheet.document.actor) {
                 await sheet.document.toChat()
             } else {
-                const actor = canvas.tokens.controlled[0]?.actor ?? game.user?.character;
-                if (actor) {
-                    await new sheet.document.constructor(sheet.document.toJSON(), { parent: actor }).toChat();
-                } else {
-                    ui.notifications.error(`You must select a token`);
-                }
+                const actor =
+                    canvas.tokens.controlled[0]?.actor ??
+                    game.user?.character ??
+                    new Actor({ name: game.user.name, type: "character" });
+
+                await new sheet.document.constructor(sheet.document.toJSON(), { parent: actor }).toChat();
             }
         },
     });
